1eb3beca4bbf4b3649b6198f0a726d396ce819b0
Christos Tamvakas <C.Tamvakas@student.tudelft.nl>
2026-06-05 05:50:44 +0200

    targeted markov now supports probabilities

--- Christos/implementation/markov.py
+++ Christos/implementation/markov.py
@@ -235,23 +235,107 @@ class TargetedMarkovModel():
     def load_data(self, data):
         self.data = data 
         
-        
         with Manager() as manager:
             shared_list = manager.list(self.chars)
             fn = partial(process_password_targeted, shared_list)
             with Pool() as pool:
                 results = pool.map(fn, self.data)
             
-        self.chain  = defaultdict(list)
-        self.starts = []
+        total_chain  = defaultdict(int)
+        total_starts = 0
+
+        self.chain  = dict()
+        self.starts = defaultdict(float)
         for chain, starts in results:
-            self.starts.extend(starts)
+            self.starts["".join(starts)] += 1
+            total_starts += 1
             for k, v in chain.items():
-                self.chain[k].extend(v)
+                k_str = "".join(k)
+                if k_str not in self.chain:
+                    self.chain[k_str] = defaultdict(float)
+                for v_ind in v:
+                    self.chain[k_str][v_ind] += 1
+                total_chain[k_str] += 1
+
+        for start in self.starts:
+            self.starts[start] /= total_starts
+        for ctx in self.chain:
+            total = total_chain[ctx]
+            for nxt in self.chain[ctx]:
+                self.chain[ctx][nxt] /= total
     
         os.makedirs('./Christos/trained_models', exist_ok=True)
         with open('./Christos/trained_models/targeted_markov.pickle', 'wb') as f:
             pickle.dump({"starts": self.starts, "chain": self.chain}, f)
+     
+    def prob_pw(self, word: str, user_data: UserData) -> float:
+
+        fn = user_data.first_name
+        ln = user_data.last_name
+        bd = user_data.birthday[0:2]
+        bm = user_data.birthday[2:4]
+        by = user_data.birthday[4:8]
+        un = user_data.username
+        em = user_data.email.split("@")[0]
+
+        tags = dict()
+        tags[self.chars[0]] = f"{fn}{ln}"
+        tags[self.chars[1]] = f"{fn[0]}{ln[0]}"
+        tags[self.chars[2]] = ln
+        tags[self.chars[3]] = fn
+        tags[self.chars[4]] = f"{fn[0]}{ln}"
+        tags[self.chars[5]] = f"{ln}{fn[0]}"
+        tags[self.chars[6]] = f"{ln[0].upper()}{ln[1:]}"
+        tags[self.chars[7]] = f"{by}{bm}{bd}"
+        tags[self.chars[8]] = f"{bm}{bd}{by}"
+        tags[self.chars[9]] = f"{bd}{bm}{by}"
+        tags[self.chars[10]] = f"{bd}{bm}"
+        tags[self.chars[11]] = by
+        tags[self.chars[12]] = f"{by}{bm}"
+        tags[self.chars[13]] = f"{bm}{by}"
+        tags[self.chars[14]] = f"{by[2:]}{bm}{bd}"
+        tags[self.chars[15]] = f"{bm}{bd}{by[2:]}"
+        tags[self.chars[16]] = f"{bd}{bm}{by[2:]}"
+        tags[self.chars[17]] = un
+        tags[self.chars[18]] = em
+        regex = re.search(r"([a-zA-Z]+)(\d+)", un)
+        if regex:
+            tags[self.chars[19]] = regex.group(1)
+            tags[self.chars[20]] = regex.group(2)
+        regex = re.search(r"([a-zA-Z]+)(\d+)", em)
+        if regex:
+            tags[self.chars[21]] = regex.group(1)
+            tags[self.chars[22]] = regex.group(2)
+
+        tokenized = tokenize_password(word, tags)
+
+        start = tokenized[:4]
+        start_key = "".join(start)
+        if start_key not in self.starts:
+            return 0.0
+
+        log_p = math.log(self.starts[start_key])
+
+        queue = deque(start, maxlen=4)
+        for ch in tokenized[4:]:
+            context_key = "".join(queue)
+            if context_key not in self.chain:
+                return 0.0
+            transitions = self.chain[context_key]
+            if ch not in transitions:
+                return 0.0
+            log_p += math.log(transitions[ch])
+            queue.append(ch)
+
+        context_key = "".join(queue)
+        if context_key not in self.chain:
+            return 0.0
+        transitions = self.chain[context_key]
+        if '\n' not in transitions:
+            return 0.0
+        log_p += math.log(transitions['\n'])
+
+        return math.exp(log_p) 
      
     def generate(self, user_data: UserData, k, seed):
         
@@ -294,29 +378,38 @@ class TargetedMarkovModel():
             tags[self.chars[21]] = regex.group(1)
             tags[self.chars[22]] = regex.group(2)
         
-        res = [user_data.password]
+        res = [[user_data.password, self.prob_pw(user_data.password, user_data)]]
+        honeywords = [user_data.password]
+
+        keys   = list(self.starts.keys())
+        probs  = list(self.starts.values())
+
         while len(res) < k-1:
-            
-            cur = rng.choice(self.starts)
+            cur = rng.choices(keys, weights=probs, k=1)[0]
             hw_parts = []
             for i in cur:
                 hw_parts.extend(tags.get(i, i))
             
-            queue = deque(cur)
-            while len(hw_parts) < len(user_data.password) and queue:
-                nxt = rng.choice(self.chain[tuple(itertools.islice(queue, 4))])
+            log_p = math.log(self.starts[cur])
+
+            queue = deque(cur, maxlen=4)
+            while queue:
+                chars = self.chain["".join(queue)]
+                nxt = rng.choices(list(chars.keys()), weights=list(chars.values()), k=1)[0]
+                
+                log_p += math.log(chars[nxt])
+                
                 if nxt == '\n':
                     break
                 
                 queue.extend(nxt)
-                queue.popleft()
-                
-                nxt = tags.get(nxt, nxt)
-                hw_parts.extend(nxt)
+                hw_parts.extend(tags.get(nxt, nxt))
+
             hw = "".join(hw_parts)
             
-            if len(hw) >= 1 and hw not in res:
-                res.append(hw)
+            if len(hw) >= 1 and hw not in honeywords:
+                res.append([hw, math.exp(log_p)])
+                honeywords.append(hw)
                 
         rng.shuffle(res)
                 
